bcfb1ecd904152f6385c9d9952d836e871fa5283
Francisco Melicias <melicias1999@gmail.com>
2023-06-22 23:31:55 +0100

    enemy spawner

--- Assets/scripts/EnemySpawner.cs
+++ Assets/scripts/EnemySpawner.cs
@@ -2,18 +2,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField]
-    private GameObject beetlePrefab;
+    private GameObject[] enemies;
+    [SerializeField]
+    private int[] percentages;
+
     //mais variaveis para cada tipo de inimigo
 
     [SerializeField]
-    private float interval = 10.5f;
+    private float interval = 1.5f;
+
+    private List<Item> mobsPercentage;
     void Start()
     {
-        StartCoroutine(spawnEnemy(interval, beetlePrefab));
+        mobsPercentage = new List<Item>();
+        for (int i = 0; i < percentages.Length; i++)
+        {
+            mobsPercentage.Add(new Item() { enemy = enemies[i],chance = percentages[i] });
+        }
+
+        StartCoroutine(spawnEnemy(interval, mobsPercentage));
     }
 
     // Update is called once per frame
@@ -22,11 +34,12 @@ public class EnemySpawner : MonoBehaviour
         
     }
 
-    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    private IEnumerator spawnEnemy(float interval, List<Item> mobsPercentage)
     {
         yield return new WaitForSeconds(interval);
-        Enemy enemyObj = Enemy.Create(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0));
-        StartCoroutine(spawnEnemy(interval, enemy));
+        Item enemyPercent = ProportionalWheelSelection.SelectItem(mobsPercentage);
+        Enemy enemyObj = Enemy.Create(enemyPercent.enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0));
+        StartCoroutine(spawnEnemy(interval, mobsPercentage));
     }
 
 }
