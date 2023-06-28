cd83e76ed7ded44cf8ff73fa0d5baa333379ea75
AlexDuqu3 <alextu12@gmail.com>
2023-06-28 01:18:40 +0100

    added settings mid game

--- Assets/VolumeSlider.cs
+++ Assets/VolumeSlider.cs
@@ -6,6 +6,12 @@ using UnityEngine.UI;
 public class VolumeSlider : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
+
+    private void Awake()
+    {
+        _slider.value = AudioListener.volume;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
